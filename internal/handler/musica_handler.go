b342f363947213ab7f1b651e2abbab2bd06b573a
Ana Ohama <lana.ohama@gmail.com>
2025-12-08 15:37:35 -0300

    Validation

--- internal/handler/musica_handler.go
+++ internal/handler/musica_handler.go
@@ -33,6 +33,30 @@ func CreateMusica(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Dados inválidos", http.StatusBadRequest)
 		return
 	}
+	if novaMusica.Titulo == "" {
+		http.Error(w, "Título obrigatório", http.StatusBadRequest)
+		return
+	}
+	if novaMusica.Artista == "" {
+		http.Error(w, "Artista obrigatório", http.StatusBadRequest)
+		return
+	}
+	if novaMusica.Album == "" {
+		http.Error(w, "Álbum obrigatório", http.StatusBadRequest)
+		return
+	}
+	if novaMusica.Ano <= 1000 || novaMusica.Ano > 2100 {
+		http.Error(w, "Ano inválido", http.StatusBadRequest)
+		return
+	}
+	if novaMusica.Genero == "" {
+		http.Error(w, "Gênero obrigatório", http.StatusBadRequest)
+		return
+	}
+	if novaMusica.Duracao <= 0 {
+		http.Error(w, "Duração inválida", http.StatusBadRequest)
+		return
+	}
 	novaMusica.ID = uuid.New().String()
 	musicas = append(musicas, novaMusica)
 	w.Header().Set("Content-Type", "application/json")
@@ -59,6 +83,30 @@ func UpdateMusica(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&musicaAtualizada)
 	if err != nil {
 		http.Error(w, "Dados inválidos", http.StatusBadRequest)
+		return
+	}
+	if musicaAtualizada.Titulo == "" {
+		http.Error(w, "Título obrigatório", http.StatusBadRequest)
+		return
+	}
+	if musicaAtualizada.Artista == "" {
+		http.Error(w, "Artista obrigatório", http.StatusBadRequest)
+		return
+	}
+	if musicaAtualizada.Album == "" {
+		http.Error(w, "Álbum obrigatório", http.StatusBadRequest)
+		return
+	}
+	if musicaAtualizada.Ano <= 1000 || musicaAtualizada.Ano > 2100 {
+		http.Error(w, "Ano inválido", http.StatusBadRequest)
+		return
+	}
+	if musicaAtualizada.Genero == "" {
+		http.Error(w, "Gênero obrigatório", http.StatusBadRequest)
+		return
+	}
+	if musicaAtualizada.Duracao <= 0 {
+		http.Error(w, "Duração inválida", http.StatusBadRequest)
 		return
 	}
 	for i, musica := range musicas {
